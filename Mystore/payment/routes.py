102fe5535ace44c80feba0af5b454e0fd7df1eff
B-toxin <142661535+B-toxin@users.noreply.github.com>
2023-09-04 08:25:18 +0100

    Update routes.py

--- Mystore/payment/routes.py
+++ Mystore/payment/routes.py
@@ -1,5 +1,5 @@
 import requests
-from flask import Flask, Blueprint, redirect, render_template, request, jsonify, session
+from flask import Flask, Blueprint, redirect, render_template, request
 from flask_wtf import CSRFProtect
 from Mystore.payment.forms import PaymentForm
 import time
@@ -12,91 +12,56 @@ csrf = CSRFProtect(app)
 payment = Blueprint('payment', __name__)
 
 
-@payment.route("/checkout", methods=['GET', 'POST'])
+@payment.route('/checkout', methods=['GET', 'POST'])
 def checkout():
-    file_identifier = request.args.get('file_identifier')
     form = PaymentForm()
-
     if form.validate_on_submit():
-        amount_in_kobo = int(form.amount.data * 100)  # Convert NGN to kobo
+        email = form.email.data
+        amount = form.amount.data
 
-        # Generate a unique reference for this payment
-        unique_reference = f'paystack_{file_identifier}_{int(time.time())}'
+        # Create a Paystack transaction
+        response = create_paystack_transaction(email, amount)
+        if response['status']:
+            authorization_url = response['data']['authorization_url']
+            return redirect(authorization_url)
+        else:
+            error_message = response['message']
+            return render_template('checkout.html', form=form, error_message=error_message)
+    return render_template('checkout.html', form=form, error_message=None)
 
-        paystack_data = {
-            'key': 'pk_live_8017ced3f46c94721ee3446267b26043297018ff',
-            # Replace with your actual public key from Paystack
-            'email': form.email.data,
-            'amount': amount_in_kobo,
-            'currency': 'NGN',
-            'ref': unique_reference,  # Use the generated unique reference
-            # Add other Paystack parameters as needed
+
+def create_paystack_transaction(email, amount):
+    try:
+        payload = {
+            "email": email,
+            "amount": int(amount * 100),  # Convert amount to kobo
+            "currency": "NGN",
+            "reference": f"paystack_example_{int(time.time())}",
+        }
+        headers = {
+            "Authorization": f"Bearer {PAYSTACK_SECRET_KEY}",
+            "Content-Type": "application/json",
         }
 
-        # Make the payment using Paystack API
-        response = requests.post('https://api.paystack.co/transaction/initialize', json=paystack_data)
-        payment_data = response.json()
+        response = requests.post("https://api.paystack.co/transaction/initialize", json=payload, headers=headers)
 
-        if payment_data.get('status') and payment_data.get('data').get('status') == 'success':
-            # Payment initiation successful, redirect to Paystack payment page
-            success_url = f'/success?file_identifier={file_identifier}'
-            return redirect(success_url)
+        response_data = response.json()
+        print(response_data)  # Print the response for inspection
 
-        # Payment initiation failed, handle the error accordingly
-    return render_template('checkout.html', form=form, file_identifier=file_identifier)
+        # Extract the authorization_url from the response_data
+        authorization_url = response_data.get("data", {}).get("authorization_url")
 
-
-@payment.route('/payment/callback', methods=['POST'])
-def payment_callback():
-    event = request.json
-    payment_reference = event['data']['reference']
-    verify_url = f'https://api.paystack.co/transaction/verify/{payment_reference}'
-    headers = {
-        'Authorization': f'Bearer {PAYSTACK_SECRET_KEY}'
-    }
-    response = requests.get(verify_url, headers=headers)
-    data = response.json()
-
-    if data['status'] and data['data']['status'] == 'success':
-        # Payment was successful, verify the payment details
-        amount_paid = data['data']['amount'] / 100  # Convert amount from kobo to NGN
-        email = data['data']['customer']['email']
-
-        # You can update your database or perform other actions here
-        session['payment_success'] = True
-        return jsonify({'message': 'Payment verified and successful'})
-    else:
-        # Payment was not successful, handle accordingly
-        return jsonify({'message': 'Payment verification failed'})
-
-
-@payment.route('/verify-payment', methods=['POST'])
-def verify_payment():
-    data = request.get_json()
-    payment_reference = data['reference']
-
-    verify_url = f'https://api.paystack.co/transaction/verify/{payment_reference}'
-    headers = {
-        'Authorization': f'Bearer {PAYSTACK_SECRET_KEY}'
-    }
-
-    response = requests.get(verify_url, headers=headers)
-    verification_data = response.json()
-
-    if verification_data.get('status') and verification_data.get('data').get('status') == 'success':
-        # Payment verification successful
-        # Update your database or perform other actions here
-        return jsonify({'message': 'Payment verified'})
-    else:
-        # Payment verification failed
-        return jsonify({'message': 'Payment verification failed'})
+        return {"status": True, "authorization_url": authorization_url}
+    except Exception as e:
+        return {"status": False, "message": str(e)}
 
 
 @payment.route('/success')
 def success():
-    file_identifier = request.args.get('file_identifier')
-    payment_success = session.get('payment_success')  # Check if payment was successful
-    if payment_success:
-        return render_template('success.html', file_identifier=file_identifier)
+    status = request.args.get('status')
+    if status == 'success':
+        # Handle logic for successful payment
+        return render_template('success.html')
     else:
-        return "Payment not successful."
+        # Handle unauthorized access or other cases
+        return redirect('/')
