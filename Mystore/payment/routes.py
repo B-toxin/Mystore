43610f75b787ba57193dd77f49536cc21d6a02d5
B-toxin <142661535+B-toxin@users.noreply.github.com>
2023-09-05 10:06:25 +0100

    Update routes.py

--- Mystore/payment/routes.py
+++ Mystore/payment/routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, Blueprint, render_template, request, redirect, url_for
+from flask import Flask, Blueprint, render_template, request, redirect
 from flask_wtf import CSRFProtect
 
 app = Flask(__name__)
@@ -6,6 +6,10 @@ app.config['SECRET_KEY'] = 'e71121f8359c7c241f56e489f91f32d7'
 csrf = CSRFProtect(app)
 
 payment = Blueprint('payment', __name__)
+
+
+def is_valid_reference(reference_id):
+    return reference_id is not None
 
 
 @payment.route('/success/usa_fb')
@@ -20,5 +24,13 @@ def usa_fb():
         return redirect('https://paystack.com/pay/usa_fb')
 
 
-def is_valid_reference(reference_id):
-    return reference_id is not None
+@payment.route('/success/ran_fb')
+def usa_fb():
+    reference_id = request.args.get('reference')
+
+    # Check if the reference ID is valid (e.g., in a database)
+    if is_valid_reference(reference_id):
+        return render_template('success/success_ran_fb.html')
+    else:
+        # Redirect or display an error message for invalid reference IDs
+        return redirect('https://paystack.com/pay/usa_fb')
