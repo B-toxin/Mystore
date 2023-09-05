455a6b203421119d4340e45ddc0ecf96ceb643c4
B-toxin <142661535+B-toxin@users.noreply.github.com>
2023-09-05 09:50:07 +0100

    Add files via upload

--- Mystore/payment/routes.py
+++ Mystore/payment/routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, Blueprint, render_template, request
+from flask import Flask, Blueprint, render_template, request, redirect, url_for
 from flask_wtf import CSRFProtect
 
 app = Flask(__name__)
@@ -9,10 +9,16 @@ payment = Blueprint('payment', __name__)
 
 
 @payment.route('/success/usa_fb')
-def success():
-    return render_template('success_usa_fb.html')
+def usa_fb():
+    reference_id = request.args.get('reference')
+
+    # Check if the reference ID is valid (e.g., in a database)
+    if is_valid_reference(reference_id):
+        return render_template('success/success_usa_fb.html')
+    else:
+        # Redirect or display an error message for invalid reference IDs
+        return redirect('https://paystack.com/pay/usa_fb')
 
 
-@payment.route('/success/ran_fb')
-def success():
-    return render_template('success_ran_fb.html')
+def is_valid_reference(reference_id):
+    return reference_id is not None
