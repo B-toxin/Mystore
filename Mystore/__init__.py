15e39f08fef798a4c4098f5432fd88a9da4ea886
B-toxin <142661535+B-toxin@users.noreply.github.com>
2023-09-06 06:51:18 +0100

    Add files via upload

--- Mystore/__init__.py
+++ Mystore/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_wtf import CSRFProtect
+from flask_sqlalchemy import SQLAlchemy
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'e71121f8359c7c241f56e489f91f32d7'
     csrf = CSRFProtect(app)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'  # Replace with your database URL
+    db = SQLAlchemy(app)
     from Mystore.pages.routes import pages
     from Mystore.products.routes import products
     from Mystore.payment.routes import payment
